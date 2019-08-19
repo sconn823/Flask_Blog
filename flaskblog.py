f4a313be908fc819924f3086e44afdd5bbe38442
Sean <sconn823@gmail.com>
2019-08-19 15:22:03 -0400

    Intial commit

---
+++ flaskblog.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import LoginForm, RegistrationForm
+
+app = Flask(__name__)
+# Random string to help encrypt logins
+app.config['SECRET_KEY'] = '354407a449af675d04fe830449b9ee9b'
+posts = [
+    {
+        'author': 'Sean Connors',
+        'title': 'Blog Post 1',
+        'date_posted': 'August 23rd',
+        'content': 'A star is born.'
+    },
+    {
+        'author': 'Jack Skellington',
+        'title': 'Blog Post 2',
+        'date_posted': 'October 31st',
+        'content': "Wouldn't you like to see something strange?"
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+# Enable this route to accept GET and POST requests, important for
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ readMe.md
@@ -0,0 +1,14 @@
+Run site on local enviornment:
+
+export FLASK_APP=flaskblog.py
+export FLASK_DEBUG=1
+
+flask run
+
+Tutorial used to build code:
+https://youtu.be/UIJKdCIEXUQ?t=1759
+
+TODO: Fix error with bracketcs in layout.html
+
+Code snippet repo:
+https://github.com/CoreyMSchafer/code_snippets/tree/master/Python/Flask_Blog/snippets
